7ebac3114c76573917143c54ce55d1b2bf5ea819
Andrew Fedchuk <andrew414414@gmail.com>
2013-10-06 16:41:09 +0300

    I renamed Targer S to TargetS

---
+++ NumberFinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using OpenCvSharp;
+
+namespace Tagrec_S
+{
+    class NumberFinder
+    {
+        public NumberFinder()
+        {
+
+        }
+
+        //public Rectangle FindRectangle(IplImage)
+        //{
+//
+  //      }
+    }
+}
